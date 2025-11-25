6767c3d2e9a164957f3a05ce028031e23fbd00a8
Prodreepto Mukherjee <110618300+prodreepto@users.noreply.github.com>
2025-11-25 13:30:54 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default {
+  base: "/bolt-diy-test/",
+};
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
